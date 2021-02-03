ec0dadc15697fb10c5c9e7ed7a4f61a062419c44
BLTerrell <brookslelandterrell@gmail.com>
2021-02-02 22:19:00 -0600

    Created query for active emplyees who will potentially retire and included current salary

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -75,3 +75,18 @@ ORDER BY de.dept_no;
 
 
 
+-- Employee Information List
+-- A list of employees with their emp_no,last_name,first_name,gender, to_date, and salary
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+
+
